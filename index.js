c220dde9a93c90f9df83983b06394a45dca03a6e
Miguel Andrade <jmandrade0@gmail.com>
2018-08-31 02:58:14 +0100

    fix default options assignment

--- index.js
+++ index.js
@@ -3,24 +3,6 @@
 module.exports = {
   name: 'ember-cli-markdown-templates',
 
-  /**
-   * Default configuration for this addon.
-   * Augments the applications configuration settings.
-   * Object returned from this hook is merged with the application's configuration object.
-   * Application's configuration always take precedence.
-   */
-  config() {
-    return {
-      'ember-cli-markdown-templates': {
-        targetHandlebars: true,
-        wrapper: false,
-        linkifyHeadings: false,
-        syntaxHighlight: false,
-        markedOptions: {}
-      }
-    };
-  },
-
   setupPreprocessorRegistry(type, registry) {
     if (type === 'parent') {
       let options = this.app.options['ember-cli-markdown-templates'];
