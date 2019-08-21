2d910c7d83809654cf2a61ba9255d31ecf5e1d9d
gossi <gossi@users.noreply.github.com>
2019-08-22 00:07:35 +0200

    parse angle-bracket components as html

--- tests-node/unit/compile-test.js
+++ tests-node/unit/compile-test.js
@@ -33,16 +33,68 @@ test('should compact code with yield', function(assert) {
 
   const html = `<h3 id="sample-h3">Sample h3</h3>
 <p>  Some text</p>
-<p>  <CodeSnippet @name="foo/bar.txt"/></p>
+  <CodeSnippet @name="foo/bar.txt"/>
+
 <p>  Second paragraph:</p>
-<p>  <Tabs as |tabs|>
+  <Tabs as |tabs|>
     <tabs.Page @title="template.hbs">
       <CodeSnippet @name="template.hbs" />
-    </tabs.Page> <tabs.Page @title="component.ts">
+    </tabs.Page>
+
+ <tabs.Page @title="component.ts">
   <CodeSnippet @name="component.ts" />
 </tabs.Page>   </Tabs>
 
-</p>
+<h3 id="another-h3">Another h3</h3>`;
+
+  assert.equal(compiled, html);
+});
+
+test('should compact code with yield and inline html in paragraph', function(assert) {
+  const code = `
+  ### Sample h3
+
+  Some text
+
+  <CodeSnippet @name="foo/bar.txt"/>
+
+  Second <i>para</i>graph with a little more
+  text and a line break. Additionally, there is also
+  a <LinkTo @route="to.the.house">link</LinkTo> embedded
+  into it.
+
+  <References class="references" as |l|>
+    <l.BlogPost
+      @title="Ember 2019: Reduce Complexity"
+      @year="2019"
+      @url="https://gos.si/blog/ember-2019-reduce-complexity"
+    as |r|>
+      <r.Author @given="Thomas" @family="Gossmann"/>
+    </l.BlogPost>
+  </References>
+
+  ### Another h3
+  `;
+  const compiled = compileMarkdown(code, {});
+
+  const html = `<h3 id="sample-h3">Sample h3</h3>
+<p>  Some text</p>
+  <CodeSnippet @name="foo/bar.txt"/>
+
+<p>  Second <i>para</i>graph with a little more
+  text and a line break. Additionally, there is also
+  a <LinkTo @route="to.the.house">link</LinkTo> embedded
+  into it.</p>
+  <References class="references" as |l|>
+    <l.BlogPost
+      @title="Ember 2019: Reduce Complexity"
+      @year="2019"
+      @url="https://gos.si/blog/ember-2019-reduce-complexity"
+    as |r|>
+      <r.Author @given="Thomas" @family="Gossmann"/>
+    </l.BlogPost>
+  </References>
+
 <h3 id="another-h3">Another h3</h3>`;
 
   assert.equal(compiled, html);
@@ -69,7 +121,8 @@ test('should compact paragraphs without yield', function(assert) {
 
   const html = `<h3 id="sample-h3">Sample h3</h3>
 <p>  Some text</p>
-<p>  <CodeSnippet @name="foo/bar.txt"/></p>
+  <CodeSnippet @name="foo/bar.txt"/>
+
 <p>  Second paragraph:</p>
   <Tabs>
     Tab content
